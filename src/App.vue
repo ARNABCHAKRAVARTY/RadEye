a2c8fc90882690f99c2c7e9c6652d182f9e02261
ArcVisionsArnabChakravarty <arnab.chakravarty@arcvisions.com>
2019-12-26 00:11:31 +0530

    initial-update

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" app>
+      <v-list dense>
+        <template v-for="item in items">
+          <v-list-item :key="item.text" :to="item.path" link>
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.text }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+        <span class="hidden-sm-and-down">RadEye</span>
+      </v-toolbar-title>
+
+      <v-spacer />
+    </v-app-bar>
+    <v-content>
+      <router-view></router-view>
+      <!-- <v-container class="fill-height" fluid></v-container> -->
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String
+  },
+  data: () => ({
+    dialog: false,
+    drawer: null,
+    items: [
+      { icon: "mdi-account", text: "Sign In", path: "/SignIn" },
+      { icon: "mdi-home", text: "Home", path: "/HelloWorld" }
+    ]
+  })
+};
+</script>
